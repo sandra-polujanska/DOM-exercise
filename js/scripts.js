da35a213cef7dcc06bae25219433209c6361661a
Sandra Połujańska <sandra.polujanska@gmail.com>
2018-03-05 18:17:12 +0100

    Add files

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 // scripts.js
 
 
+var list = document.getElementById('list');
+var add = document.getElementById('addElem');
+
+add.addEventListener('click', function() {
+    
+    var element = document.createElement('li');
+    var number = document.getElementsByTagName('li');
+    element.innerHTML = 'item ' + number.length;
+    list.appendChild(element);
+});
+ 
